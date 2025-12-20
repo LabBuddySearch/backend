e3cc1b388734ceb299c96d947832766314055ccf
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2025-12-20 15:49:36 +0300

    add findbyautorid

--- src/main/java/org/example/repository/CardRepository.java
+++ src/main/java/org/example/repository/CardRepository.java
@@ -3,8 +3,6 @@ package org.example.repository;
 import org.example.model.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, UUID>, JpaSpecificationExecutor<Card> {
-    @Query("SELECT c FROM Card c " +
-            "JOIN Like l ON c.id = l.cardId.id " +
-            "WHERE l.userId.id = :userId ")
-    List<Card> findLikedCardsByUserId(@Param("userId") UUID userId);
+    List<Card> findAllByAuthorId_Id(UUID authorId);
 }
